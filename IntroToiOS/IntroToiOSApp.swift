4c6a5cf74ce0e367d7b52337a7b9bbdd5e2ef2b7
Florian Schweizer <florian.schweizer@tum.de>
2025-05-13 15:34:41 +0200

    Initial Commit

---
+++ IntroToiOS/IntroToiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToiOSApp.swift
+//  IntroToiOS
+//
+//  Created by Florian Schweizer on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct IntroToiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
